5909f96441ebc4b17989c920779c14a2219ba12b
Ilya Vahtykov <ilya.v@profi.io>
2025-01-27 21:14:41 +0300

    set file limits

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,28 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module.js';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as express from 'express';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // Add middleware to handle OPTIONS requests before CORS
-  app.use((req, res, next) => {
-    if (req.method === 'OPTIONS') {
-      res.status(200).end();
-      return;
-    }
-    next();
-  });
+  // app.use((req, res, next) => {
+  //   if (req.method === 'OPTIONS') {
+  //     res.status(200).end();
+  //     return;
+  //   }
+  //   next();
+  // });
+
+  app.use(json({ limit: '100mb' }));
+  app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 
   // Enable CORS with specific configuration
   app.enableCors({
     origin: true, // разрешает все origins для тестирования
-    credentials: true
+    credentials: true,
+    // preflightContinue: false,
   });
 
   app.useGlobalPipes(new ValidationPipe({
